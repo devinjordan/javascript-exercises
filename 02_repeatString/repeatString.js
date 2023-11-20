15df93e23ecf634930e41a33ac16b6f63253dce4
devinjordan <134808091+devinjordan@users.noreply.github.com>
2023-11-20 18:31:57 -0500

    Solve 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return('ERROR');
+  }
+  if (string) {
+    let returnValue = '';
+    for (i = 0; i < num; i++) {
+      returnValue += string;
+    }
+    return returnValue;
+  } else {
+    return('');
+  }
 };
 
 // Do not edit below this line
