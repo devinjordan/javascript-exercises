faf5efa08d53930a0ec52150688b7cab8c20b19b
devinjordan <134808091+devinjordan@users.noreply.github.com>
2023-11-20 18:57:39 -0500

    Solve 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let array = string.split("");
+  let revString = array.reverse();
+  return revString.join('');
 };
 
 // Do not edit below this line
