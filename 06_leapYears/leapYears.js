5950790141aca5f860311a5d027b352b37e00490
devinjordan <134808091+devinjordan@users.noreply.github.com>
2023-11-20 20:01:35 -0500

    Solve 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  switch (year % 100 === 0) {
+    case (false):
+      if (year % 4 === 0) {
+        return(true);
+      } else {
+        return(false);
+      };
+    case (true):
+      if (year % 400 === 0) {
+        return (true);
+      } else {
+        return (false);
+      };
+  };
 };
 
 // Do not edit below this line
