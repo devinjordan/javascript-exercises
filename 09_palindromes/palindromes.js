59a1935da420cabc2c38f89f81f2ee8d1104ff88
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-01-15 14:26:13 -0500

    Stack OF 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, ""); // XOR a-z, 0-9 globally, replace with nothing
+  return processedString.split("").reverse().join("") == processedString;
 };
 
-// Do not edit below this line
 module.exports = palindromes;
