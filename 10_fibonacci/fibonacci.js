91c725db81b7e52bf683a5c2d2b24183827303cf
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-01-15 15:10:14 -0500

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (typeof num == 'number' || typeof num == 'string') {
+    if (num < 0) return "OOPS";
+    let fibNums = [];
+    for (i = 0; i < num; i++) {
+      if (i == 0 || i == 1) {
+        fibNums[i] = 1;
+        continue;
+      }
+      fibNums[i] = fibNums[i - 2] + fibNums[i - 1];
+    }
+    console.log(fibNums);
+    console.log(fibNums[num - 1]);
+    return fibNums[num - 1];
+  } else {
+    console.log('Usage error. Must be a positive integer.');
+    return;
+  }
 
 };
 
