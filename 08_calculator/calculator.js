05bdc4ebc8d7d0861aa5c5b4dd4d592cefaf8ba3
devinjordan <134808091+devinjordan@users.noreply.github.com>
2024-01-15 10:37:34 -0500

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  array.forEach((item) => total += item);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  array.forEach((item) => total *= item);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+  while (a != 0) {
+    total *= a;
+    a--;
+  }
+  return total;
 };
 
 // Do not edit below this line
